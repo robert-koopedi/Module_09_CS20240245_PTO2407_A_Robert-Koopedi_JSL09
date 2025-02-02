65a7650d59495522373a3128ee8a065869243618
robert koopedi <koopedi05@gmail.com>
2025-02-02 17:31:35 +0200

    default background image if unsplash fetch fails

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
   })
   .catch(err => {
     console.log("Something went wrong!");
-  
+    // Default background image
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1539593395743-7da5ee10ff07?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MzgyMjU0OTB8&ixlib=rb-4.0.3&q=80&w=1080)`;
   });
 
